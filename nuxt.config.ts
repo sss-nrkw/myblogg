65433ea687bab349f858ff39bc39e8607657c16c
nari0 <narinari3579@gmail.com>
2023-10-29 01:44:50 +0900

    12

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,14 @@ const { API_KEY, SERVICE_DOMAIN } = process.env;
 
 export default defineNuxtConfig({
     srcDir: 'src/',
+		nitro: {
+			prerender: {
+				crawlLinks: true
+			}
+		},
     runtimeConfig: {
         apiKey: API_KEY,
         serviceDomain: SERVICE_DOMAIN
     },
     css: ['~/assets/css/reset.css', '~/assets/css/style.css',],
-})
-
-defineNuxtConfig({
-  nitro: {
-    prerender: {
-      crawlLinks: true
-    }
-  }
 })

1d05d5965f6fb6101568c8d3c26e4e117ccc1e1f
nari0 <narinari3579@gmail.com>
2023-10-28 23:32:38 +0900

    7

--- src/pages/[slug]/index.vue
+++ src/pages/[slug]/index.vue
@@ -40,6 +40,35 @@ if (!article.value) {
     font-size: 16px;
     line-height: 1.6;
 }
+.md:deep(*) {
+    margin-top: 0;
+    margin-bottom: 2rem;
+    line-height: 1.9;
+    font-size: 1.6rem;
+    font-weight: 500;
+}
+
+.md:deep(p) code {
+    background-color: #eee;
+    color: #333;
+    padding: 0.2em 0.4em;
+    font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace;
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
+}
+
+.md:deep(pre) {
+    line-height: 1.2;
+    background-color: #1D1F21;
+    padding: 2% 4%;
+    overflow-x: scroll;
+}
+
+.md:deep(ul) {
+    list-style-type: disc;
+}
+
+
 
 /* You can add more styles as needed */
 </style>

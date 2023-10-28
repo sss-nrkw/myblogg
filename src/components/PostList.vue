81a7e544254fdee9f635919b6e4f28cf77c7d32e
nari0 <narinari3579@gmail.com>
2023-10-29 01:24:05 +0900

    10

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,4 +1,3 @@
-<!-- src/components/PostList.vue -->
 <script setup lang="ts">
 import { Post } from '../types/blog'
 
@@ -7,54 +6,45 @@ type Props = {
 }
 
 const { posts } = defineProps<Props>()
-
 </script>
 
 <template>
-    <div>
-        <article class="article" v-for="post in posts" :key="post.id">
-            <span class="published">{{ post.publishedAt }}</span>
-            <span v-for="tag in post.tag" :key="tag.id" class="tag">{{ tag.name }}</span>
-            <nuxt-link :to="`/${post.id}`">
-                <h1 class="title">
-                    {{ post.title }}
-                </h1>
-            </nuxt-link>
-        </article>
-    </div>
+  <div>
+    <article class="article" v-for="post in posts" :key="post.id">
+      <span class="published">{{ post.publishedAt }}</span>
+      <span class="tag" v-for="tag in post.tags" :key="tag.id">{{ tag.name }}</span>
+      <nuxt-link :to="`/${post.id}`">
+        <h1 class="title">{{ post.title }}</h1>
+      </nuxt-link>
+    </article>
+  </div>
 </template>
 
 <style scoped>
 .article {
-    margin-top: 1rem;
-    margin-bottom: 6rem;
-    width: 100%;
-    align-items: center;
+  margin-top: 1rem;
+  margin-bottom: 6rem;
+  width: 100%;
+  align-items: center;
 }
 
 .published {
-    font-size: 1.4rem;
-    color: #888;
-    margin-right: 20px;
-}
-
-.title {
-    margin-top: 6px;
-    font-size: 2.8rem;
-    color: #0d1a3c;
-    line-height: 1.6;
-    font-weight: bold;
-}
-
-.title:hover {
-    opacity: .5;
+  font-size: 1.4rem;
+  color: #888;
+  margin-right: 20px;
 }
 
 .tag {
-    font-size: 1.4rem;
-    color: 888;
-    opacity: 0.7;
-    letter-spacing: 1px;
-    margin-right: 1rem;
+  font-size: 1.4rem;
+  color: #0d1a3c;
+  margin-right: 10px;
+}
+
+.title {
+  margin-top: 6px;
+  font-size: 2.8rem;
+  color: #0d1a3c;
+  line-height: 1.6;
+  font-weight: bold;
 }
 </style>
